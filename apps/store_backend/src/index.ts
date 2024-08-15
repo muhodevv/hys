63a1c6b4e135aac824cc41d8583fe0894fd24dda
muho <hi@muho.dev>
2024-08-15 13:55:42 +0300

    Add other projects

---
+++ apps/store_backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+});
+
+app.post('/users', async (req, res) => {
+});
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
